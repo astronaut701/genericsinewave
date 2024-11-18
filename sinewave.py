7548a22a55d18b488c49e8bf1789bee7b1bc5f55
Astronaut701 <chris.mail701@gmail.com>
2024-11-17 22:19:46 -0500

    Add comments.

--- sinewave.py
+++ sinewave.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import math
 
+# Create an empty list for the points for our axes
 x = []
 y = []
+# Generates 200 points on a sine wave
 for i in range(200):
     x.append(i)
     y.append(math.sin(2 * math.pi * i / 10))
 
+# Sets up x and y plot, graph title and axis names.
 plt.plot(x, y)
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
